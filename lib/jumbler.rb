fc38195f49aa6d971b1b1fcf48a87edbc1af8999
Adam Cox <adamdama@hotmail.com>
2012-12-16 22:57:17 +0000

    js files added to list

--- lib/jumbler.rb
+++ lib/jumbler.rb
@@ -9,23 +9,49 @@
 #
 
 require 'listen'
+require 'find'
 
 class Jumbler
   
   def initialize args
-    @args = args
+   # @args = args
     @current_dir = Dir.pwd
-
-    @listener = Listen.to(@current_dir, :relative_paths => true, :filter => /\.js$|\.rb$/, :ignore => %r{ignored/path/}) do |modified, added, removed|
-      puts modified.inspect
-      puts added.inspect
-      puts removed.inspect
+    @watch_dir = @current_dir
+    # get all js files to be comiled
+    @js_file_paths = self.get_js @watch_dir
+    
+    # listen to the specified folder for changes to js files
+    @listener = Listen.to(@watch_dir, :relative_paths => true, :filter => /\.js$/, :ignore => %r{ignored/path/}) do |modified, added, removed|
+      # if a new file is added we need to add it to the array
+      added.collect{|item| 
+        @js_file_paths << @watch_dir + File::SEPARATOR + item
+      }
+      
+      # compile the new js file
+      self.jumble      
     end
+  end
+  
+  # recursively finds all js files from a specified directory
+  def get_js root_dir
+    file_paths = []
+    
+    Find.find(root_dir) do |path|
+      file_paths << path if path =~ /.*\.js$/
+    end
+    
+    return file_paths
+  end
+  
+  # creates the command to be used to run the closure compiler
+  def jumble
+    puts @js_file_paths
   end
 end
 
-
+# create the jumbler
 j = Jumbler.new ARGV
+
 =begin
 def help
   print "
