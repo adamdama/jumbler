1967c72b08c35148ff0ff1432c5f0d4c96fdab00
Adam Cox <adamdama@hotmail.com>
2012-12-16 23:36:19 +0000

    Files now removed from list.

--- lib/jumbler.rb
+++ lib/jumbler.rb
@@ -12,37 +12,52 @@ require 'listen'
 require 'find'
 
 class Jumbler
+  @ds = File::SEPARATOR
   
+  # initialize jumbler
   def initialize args
-   # @args = args
+    # @args = args
     @current_dir = Dir.pwd
     @watch_dir = @current_dir
     # get all js files to be comiled
     @js_file_paths = self.get_js @watch_dir
-    
+
     # listen to the specified folder for changes to js files
     @listener = Listen.to(@watch_dir, :relative_paths => true, :filter => /\.js$/, :ignore => %r{ignored/path/}) do |modified, added, removed|
-      # if a new file is added we need to add it to the array
-      added.collect{|item| 
-        @js_file_paths << @watch_dir + File::SEPARATOR + item
-      }
+      if(added.length > 0)
+        # if a new file is added we need to add it to the array
+        added.collect{|item|
+          @js_file_paths << @watch_dir + File::SEPARATOR + item
+        }
+      end
+      
+      if(removed.length > 0)
+        # if a file is removed we need to remove it from the array
+        removed.collect{|item|
+          @js_file_paths.any? {|path|
+            if(path == @watch_dir + File::SEPARATOR + item)
+              @js_file_paths.delete(path)
+            end
+           } 
+        }
+      end
       
       # compile the new js file
-      self.jumble      
+      self.jumble
     end
   end
-  
+
   # recursively finds all js files from a specified directory
   def get_js root_dir
     file_paths = []
-    
+
     Find.find(root_dir) do |path|
       file_paths << path if path =~ /.*\.js$/
     end
-    
+
     return file_paths
   end
-  
+
   # creates the command to be used to run the closure compiler
   def jumble
     puts @js_file_paths
@@ -54,15 +69,15 @@ j = Jumbler.new ARGV
 
 =begin
 def help
-  print "
-  Usage: #{__FILE__} watch --in_folder --out_folder
-  "
+print "
+Usage: #{__FILE__} watch --in_folder --out_folder
+"
 end
 
 if ARGV.empty?
-  help
-  exit
+help
+exit
 else
-  j = Jumbler.new ARGV
+j = Jumbler.new ARGV
 end
 =end
